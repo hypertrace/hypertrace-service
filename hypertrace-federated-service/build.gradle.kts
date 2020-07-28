d594028b0b10875e65400c21089f0da6bcbf775f
samarth-gupta-traceable <samarth@traceable.ai>
2020-07-28 18:13:39 +0530

    Update platform service framework to 0.1.4

--- hypertrace-federated-service/build.gradle.kts
+++ hypertrace-federated-service/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
   implementation("org.hypertrace.gateway.service:gateway-service")
   implementation("org.hypertrace.gateway.service:gateway-service-impl")
 
-  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.3")
+  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.4")
   implementation("org.hypertrace.core.grpcutils:grpc-server-utils:0.1.3")
   implementation("org.hypertrace.core.documentstore:document-store:0.1.1")
 
