191336d21fac2c0dbbf23f1a6586a9286b70d2d8
Adrian Cole <adriancole@users.noreply.github.com>
2020-08-18 09:55:08 +0800

    Locks Java to 11 (#14)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,11 @@ plugins {
 
 subprojects {
   group = "org.hypertrace.core.bootstrapper"
+
+  pluginManager.withPlugin("java") {
+    configure<JavaPluginExtension> {
+      sourceCompatibility = JavaVersion.VERSION_11
+      targetCompatibility = JavaVersion.VERSION_11
+    }
+  }
 }
