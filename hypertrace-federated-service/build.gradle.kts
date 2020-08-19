882620ac97be96c946bce0c178c780db59cc0bd4
Adrian Cole <adriancole@users.noreply.github.com>
2020-08-19 15:52:54 +0800

    Updates to latest docker plugin inlining port and healthcheck config (#14)
    after:
    ```
    hypertrace-federated-service   java -cp /app/resources:/a ...   Up (healthy)   9001/tcp, 9002/tcp                                                               ```

--- hypertrace-federated-service/build.gradle.kts
+++ hypertrace-federated-service/build.gradle.kts
@@ -38,6 +38,15 @@ application {
   mainClassName = "org.hypertrace.core.serviceframework.PlatformServiceLauncher"
 }
 
+hypertraceDocker {
+  defaultImage {
+    javaApplication {
+      serviceName.set("${project.name}")
+      port.set(9001)
+    }
+  }
+}
+
 // Config for gw run to be able to run this locally. Just execute gw run here on Intellij or on the console.
 tasks.run<JavaExec> {
   jvmArgs = listOf("-Dbootstrap.config.uri=file:${project.buildDir}/resources/main/configs", "-Dservice.name=${project.name}")
