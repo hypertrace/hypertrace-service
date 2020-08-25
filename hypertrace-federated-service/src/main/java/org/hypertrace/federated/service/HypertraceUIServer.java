482c76ab31dc0331e25817b602ed5a0d4d4c5b67
Ronak <ronak@traceable.ai>
2020-08-25 15:06:03 +0530

    fixed reload issue

--- hypertrace-federated-service/src/main/java/org/hypertrace/federated/service/HypertraceUIServer.java
+++ hypertrace-federated-service/src/main/java/org/hypertrace/federated/service/HypertraceUIServer.java
@@ -3,11 +3,18 @@ package org.hypertrace.federated.service;
 import com.typesafe.config.Config;
 import java.net.URI;
 import java.net.URL;
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.handler.DefaultHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 import org.hypertrace.graphql.service.GraphQlServiceImpl;
 import org.slf4j.Logger;
@@ -31,11 +38,45 @@ public class HypertraceUIServer {
     ResourceHandler resourceHandler = new ResourceHandler();
     resourceHandler.setBaseResource(getBaseResource());
 
-    HandlerList handlers = new HandlerList();
-    handlers.setHandlers(new Handler[]{resourceHandler, graphQlService.getContextHandler(),
-            new DefaultHandler()});
+    ContextHandler baseContextHandler = new ContextHandler();
+    baseContextHandler.setContextPath("/");
+    baseContextHandler.setHandler(resourceHandler);
 
-    server.setHandler(handlers);
+    ContextHandler homeContextHandler = new ContextHandler();
+    homeContextHandler.setContextPath("/home");
+    homeContextHandler.setHandler(resourceHandler);
+
+    ContextHandler applicationFlowContextHandler = new ContextHandler();
+    applicationFlowContextHandler.setContextPath("/application-flow");
+    applicationFlowContextHandler.setHandler(resourceHandler);
+
+    ContextHandler servicesContextHandler = new ContextHandler();
+    servicesContextHandler.setContextPath("/services");
+    servicesContextHandler.setHandler(resourceHandler);
+
+    ContextHandler backendsContextHandler = new ContextHandler();
+    backendsContextHandler.setContextPath("/backends");
+    backendsContextHandler.setHandler(resourceHandler);
+
+    ContextHandler explorerContextHandler = new ContextHandler();
+    explorerContextHandler.setContextPath("/explorer");
+    explorerContextHandler.setHandler(resourceHandler);
+
+    ContextHandlerCollection contexts = new ContextHandlerCollection();
+    contexts.setHandlers(new Handler[] {
+            baseContextHandler,
+            homeContextHandler,
+            applicationFlowContextHandler,
+            servicesContextHandler,
+            backendsContextHandler,
+            explorerContextHandler,
+            graphQlService.getContextHandler()});
+
+//    HandlerList handlers = new HandlerList();
+//    handlers.setHandlers(new Handler[]{resourceHandler, graphQlService.getContextHandler(),
+//            new DefaultHandler()});
+
+    server.setHandler(contexts);
     server.setStopAtShutdown(true);
   }
 
