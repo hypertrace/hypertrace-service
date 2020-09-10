91d56a63aa267df404191a2ee91b866b65be6fef
Adrian Cole <adrian.f.cole@gmail.com>
2020-09-10 09:29:54 +0800

    Moving docker plugin to latest
    New functionality isn't used as the docker setup is custom here.
    However, this might help tell the difference between what is failing
    on PR branch vs main
    
    See #26

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
   id("org.hypertrace.repository-plugin") version "0.2.3"
   id("org.hypertrace.ci-utils-plugin") version "0.1.4"
-  id("org.hypertrace.docker-java-application-plugin") version "0.7.0" apply false
-  id("org.hypertrace.docker-publish-plugin") version "0.7.0" apply false
+  id("org.hypertrace.docker-java-application-plugin") version "0.7.1" apply false
+  id("org.hypertrace.docker-publish-plugin") version "0.7.1" apply false
   id("org.hypertrace.jacoco-report-plugin") version "0.1.3" apply false
 }
 
