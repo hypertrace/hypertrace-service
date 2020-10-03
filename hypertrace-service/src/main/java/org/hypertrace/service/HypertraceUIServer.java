85c835e918dcc9b2572004cd2dc148ffe5d6b662
kotharironak <53209990+kotharironak@users.noreply.github.com>
2020-10-03 11:17:10 +0530

    redirecting to index.html for deep paths (#34)
    * wip
    
    * fixing broken link issues on reload
    
    * Update hypertrace-service/build.gradle.kts
    
    * removed un-used files, and reformat codes

--- hypertrace-service/src/main/java/org/hypertrace/service/HypertraceUIServer.java
+++ hypertrace-service/src/main/java/org/hypertrace/service/HypertraceUIServer.java
@@ -4,11 +4,10 @@ import com.typesafe.config.Config;
 import java.net.URI;
 import java.net.URL;
 import java.util.Timer;
-import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandler;
-import org.eclipse.jetty.server.handler.ContextHandlerCollection;
-import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ErrorPageErrorHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.util.resource.Resource;
 import org.hypertrace.graphql.service.GraphQlServiceImpl;
 import org.slf4j.Logger;
@@ -18,6 +17,7 @@ import org.slf4j.LoggerFactory;
  * Serves both the Hypertrace UI and GraphQL APIs used by it.
  */
 public class HypertraceUIServer {
+
   private static final Logger LOGGER = LoggerFactory.getLogger(HypertraceUIServer.class);
 
   private static final String PORT_CONFIG = "hypertraceUI.port";
@@ -38,44 +38,16 @@ public class HypertraceUIServer {
     server = new Server(port);
     graphQlService = new GraphQlServiceImpl(graphQlServiceAppConfig);
 
-    ResourceHandler resourceHandler = new ResourceHandler();
-    resourceHandler.setBaseResource(getBaseResource());
+    ServletContextHandler servletContextHandler = graphQlService.getContextHandler();
+    servletContextHandler.setBaseResource(getBaseResource());
+    servletContextHandler.setWelcomeFiles(new String[]{"index.html"});
+    servletContextHandler.addServlet(DefaultServlet.class, "/");
 
-    ContextHandler baseContextHandler = new ContextHandler();
-    baseContextHandler.setContextPath("/");
-    baseContextHandler.setHandler(resourceHandler);
+    ErrorPageErrorHandler errorHandler = new ErrorPageErrorHandler();
+    errorHandler.addErrorPage(404, "/"); // return root ... being index.html
+    servletContextHandler.setErrorHandler(errorHandler);
 
-    ContextHandler homeContextHandler = new ContextHandler();
-    homeContextHandler.setContextPath("/home");
-    homeContextHandler.setHandler(resourceHandler);
-
-    ContextHandler applicationFlowContextHandler = new ContextHandler();
-    applicationFlowContextHandler.setContextPath("/application-flow");
-    applicationFlowContextHandler.setHandler(resourceHandler);
-
-    ContextHandler servicesContextHandler = new ContextHandler();
-    servicesContextHandler.setContextPath("/services");
-    servicesContextHandler.setHandler(resourceHandler);
-
-    ContextHandler backendsContextHandler = new ContextHandler();
-    backendsContextHandler.setContextPath("/backends");
-    backendsContextHandler.setHandler(resourceHandler);
-
-    ContextHandler explorerContextHandler = new ContextHandler();
-    explorerContextHandler.setContextPath("/explorer");
-    explorerContextHandler.setHandler(resourceHandler);
-
-    ContextHandlerCollection contexts = new ContextHandlerCollection();
-    contexts.setHandlers(new Handler[]{
-            baseContextHandler,
-            homeContextHandler,
-            applicationFlowContextHandler,
-            servicesContextHandler,
-            backendsContextHandler,
-            explorerContextHandler,
-            graphQlService.getContextHandler()});
-
-    server.setHandler(contexts);
+    server.setHandler(graphQlService.getContextHandler());
     server.setStopAtShutdown(true);
   }
 
@@ -99,11 +71,12 @@ public class HypertraceUIServer {
 
     String defaultTenant = graphQlServiceAppConfig.getString(DEFAULT_TENANT_ID_CONFIG);
     HypertraceUIServerTimerTask timerTask = new HypertraceUIServerTimerTask(appConfig,
-            this, defaultTenant);
-    LOGGER.info(String.format("Starting a timer task for checking health for bootstrapping process, " +
+        this, defaultTenant);
+    LOGGER.info(
+        String.format("Starting a timer task for checking health for bootstrapping process, " +
             "will try first attempt after [%s] seconds", timerTask.getStartPeriod()));
     new Timer().scheduleAtFixedRate(timerTask, timerTask.getStartPeriod() * 1000,
-            timerTask.getInterval() * 1000);
+        timerTask.getInterval() * 1000);
   }
 
   public void start() {
