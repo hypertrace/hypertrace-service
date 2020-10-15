7a10e0bb91cb732d60929ef96884420497028c51
Ronak <ronak@traceable.ai>
2020-10-15 19:31:20 +0530

    changes the thirdparty log level, and healthcheck is done in next cycle

--- hypertrace-service/src/main/java/org/hypertrace/service/HypertraceUIServerTimerTask.java
+++ hypertrace-service/src/main/java/org/hypertrace/service/HypertraceUIServerTimerTask.java
@@ -24,6 +24,7 @@ import org.slf4j.LoggerFactory;
  * Helper TimerTask for checking health of dependency data services before starting UI server
  */
 public class HypertraceUIServerTimerTask extends TimerTask {
+
   private static final Logger LOGGER = LoggerFactory.getLogger(HypertraceUIServerTimerTask.class);
 
   private static final String RETRIES_CONFIG = "hypertraceUI.init.waittime.retries";
@@ -41,9 +42,6 @@ public class HypertraceUIServerTimerTask extends TimerTask {
   private static final String PINOT_CONTROLLER_PORT = "pinot.controller_port";
   private static final int DEFAULT_PINOT_CONTROLLER_PORT = 9000;
 
-  private static final String PINOT_BROKER_PORT = "pinot.broker_port";
-  private static final int DEFAULT_PINOT_BROKER_PORT = 8099;
-
   private final HypertraceUIServer uiServer;
   private final GatewayServiceBlockingStub client;
   private int numRetries;
@@ -57,33 +55,35 @@ public class HypertraceUIServerTimerTask extends TimerTask {
   private int pinotServerPort;
   private int pinotControllerPort;
   private boolean isPinotUp;
-  private int afterPinotUpCount;
 
 
-  public HypertraceUIServerTimerTask(Config appConfig, HypertraceUIServer uiServer, String defaultTenant) {
-    maxRetries = appConfig.hasPath(RETRIES_CONFIG) ? appConfig.getInt(RETRIES_CONFIG) : DEFAULT_RETRIES;
-    timeout = appConfig.hasPath(TIMEOUT_CONFIG) ? appConfig.getInt(TIMEOUT_CONFIG) : DEFAULT_TIMEOUT;
+  public HypertraceUIServerTimerTask(Config appConfig, HypertraceUIServer uiServer,
+      String defaultTenant) {
+    maxRetries =
+        appConfig.hasPath(RETRIES_CONFIG) ? appConfig.getInt(RETRIES_CONFIG) : DEFAULT_RETRIES;
+    timeout =
+        appConfig.hasPath(TIMEOUT_CONFIG) ? appConfig.getInt(TIMEOUT_CONFIG) : DEFAULT_TIMEOUT;
     interval = appConfig.hasPath(INTERVAL) ? appConfig.getInt(INTERVAL) : DEFAULT_INTERVAL;
-    startPeriod = appConfig.hasPath(START_PERIOD) ? appConfig.getInt(START_PERIOD) : DEFAULT_START_PERIOD;
+    startPeriod =
+        appConfig.hasPath(START_PERIOD) ? appConfig.getInt(START_PERIOD) : DEFAULT_START_PERIOD;
 
     pinotSeverHost = appConfig.hasPath(PINOT_SERVER_HOST) ?
-            appConfig.getString(PINOT_SERVER_HOST) : DEFAULT_PINOT_SERVER_HOST;
+        appConfig.getString(PINOT_SERVER_HOST) : DEFAULT_PINOT_SERVER_HOST;
     pinotServerPort = appConfig.hasPath(PINOT_SERVER_PORT) ?
-            appConfig.getInt(PINOT_SERVER_PORT) : DEFAULT_PINOT_SERVER_PORT;
+        appConfig.getInt(PINOT_SERVER_PORT) : DEFAULT_PINOT_SERVER_PORT;
     pinotControllerPort = appConfig.hasPath(PINOT_CONTROLLER_PORT) ?
-            appConfig.getInt(PINOT_CONTROLLER_PORT) : DEFAULT_PINOT_CONTROLLER_PORT;
+        appConfig.getInt(PINOT_CONTROLLER_PORT) : DEFAULT_PINOT_CONTROLLER_PORT;
 
     this.uiServer = uiServer;
     this.numRetries = 0;
     this.defaultTenant = defaultTenant;
     this.startTime = Instant.now().toEpochMilli();
     this.isPinotUp = false;
-    this.afterPinotUpCount = 0;
 
     client = GatewayServiceGrpc.newBlockingStub(ManagedChannelBuilder.forAddress(
-            "localhost", appConfig.getInt("service.port")).usePlaintext().build())
-            .withCallCredentials(RequestContextClientCallCredsProviderFactory
-                    .getClientCallCredsProvider().get());
+        "localhost", appConfig.getInt("service.port")).usePlaintext().build())
+        .withCallCredentials(RequestContextClientCallCredsProviderFactory
+            .getClientCallCredsProvider().get());
   }
 
   public long getStartPeriod() {
@@ -99,44 +99,49 @@ public class HypertraceUIServerTimerTask extends TimerTask {
     try {
       if (numRetries >= maxRetries) {
         cancel();
-        LOGGER.info(String.format("Max out attempts [%s] in checking bootstrapping status. Manually check " +
+        LOGGER.info(String
+            .format("Max out attempts [%s] in checking bootstrapping status. Manually check " +
                 "the status of data service [pinot].", numRetries));
         uiServer.start();
         return;
       }
 
-      if (executePinotHealthCheck()
-          && executePinotControllerHealthCheck()
-          && executePinotBrokerHealthCheck()
-          && executeBrokerRegistrationCheck()) {
+      /**
+       * First we will check for pinot, and in follow up run, we will try out span requests.
+       * Once brokers are up, and register with controller, it also has to updated in
+       * zookeeper.
+       * */
+      if (!isPinotUp && executePinotHealthCheck() && executeBrokerRegistrationCheck()) {
         isPinotUp = true;
-        afterPinotUpCount++;
-        LOGGER.warn(String.format("Finished an attempt [%s] in checking for bootstrapping status. " +
-            "It seems dependent data service [pinot] is up. " +
-            "will check for span status after [%s] seconds", numRetries, interval));
-      } else {
-        LOGGER.warn(String.format("Finished an attempt [%s] in checking for bootstrapping status. " +
-            "It seems dependent data service [pinot] is not yet up. " +
-            "will retry after [%s] seconds", numRetries, interval));
+        LOGGER
+            .warn(String.format("Finished an attempt [%s] in checking for bootstrapping status. " +
+                "It seems dependent data service [pinot] is up now. " +
+                "will check for span status after [%s] seconds", numRetries, interval));
+        return;
+      } else if (!isPinotUp) {
+        LOGGER
+            .warn(String.format("Finished an attempt [%s] in checking for bootstrapping status. " +
+                "It seems dependent data service [pinot] is not yet up. " +
+                "will retry after [%s] seconds", numRetries, interval));
         return;
       }
 
-      if (afterPinotUpCount > 1 && executeHealthCheck()) {
+      if (executeHealthCheck()) {
         cancel();
         LOGGER.info(String.format("Stack is up after [%s] attempts, and duration [%s] in millis.",
-                numRetries, Instant.now().toEpochMilli() - startTime));
+            numRetries, Instant.now().toEpochMilli() - startTime));
         uiServer.start();
         return;
       }
 
       LOGGER.warn(String.format("Finished an attempt [%s] in checking for bootstrapping status. " +
-              "It seems dependent data service [pinot] is not yet up. " +
-              "will retry after [%s] seconds", numRetries, interval));
+          "It seems brokers have not been registered with zookeeper " +
+          "will retry after [%s] seconds", numRetries, interval));
 
     } catch (Exception ex) {
       LOGGER.warn(String.format("Finished an attempt [%s] in checking for bootstrapping status. " +
-              "It seems dependent data service [pinot] is not yet up. " +
-              "will retry after [%s] seconds", numRetries, interval));
+              "There was an exception in checking status, will retry after [%s] seconds",
+          numRetries, interval));
     } finally {
       numRetries++;
     }
@@ -144,65 +149,24 @@ public class HypertraceUIServerTimerTask extends TimerTask {
 
   private boolean executeHealthCheck() {
     SpansResponse response = GrpcClientRequestContextUtil.executeInTenantContext(defaultTenant,
-            () -> client.withDeadline(Deadline.after(timeout, TimeUnit.SECONDS))
-                    .getSpans(buildSpanRequest()));
+        () -> client.withDeadline(Deadline.after(timeout, TimeUnit.SECONDS))
+            .getSpans(buildSpanRequest()));
     return response.getSpansCount() >= 0;
   }
 
   private SpansRequest buildSpanRequest() {
     return SpansRequest.newBuilder()
-            .setStartTimeMillis(System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(10))
-            .setEndTimeMillis(System.currentTimeMillis())
-            .addSelection(QueryExpressionUtil.getColumnExpression("EVENT.id"))
-            .setLimit(1)
-            .build();
+        .setStartTimeMillis(System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(10))
+        .setEndTimeMillis(System.currentTimeMillis())
+        .addSelection(QueryExpressionUtil.getColumnExpression("EVENT.id"))
+        .setLimit(1)
+        .build();
   }
 
   private boolean executePinotHealthCheck() throws Exception {
     HttpURLConnection con = null;
     try {
       URL url = new URL(String.format("http://%s:%s/health", pinotSeverHost, pinotServerPort));
-      LOGGER.info("checking pinot server health:{}", url.toString());
-      con = (HttpURLConnection) url.openConnection();
-      con.setRequestMethod("GET");
-      con.setConnectTimeout(timeout * 1000);
-      con.setReadTimeout(timeout * 1000);
-      con.connect();
-      int status = con.getResponseCode();
-
-      return status >= 200 && status <= 206;
-    } finally {
-      if (con != null) {
-        con.disconnect();
-      }
-    }
-  }
-
-  private boolean executePinotControllerHealthCheck() throws Exception {
-    HttpURLConnection con = null;
-    try {
-      URL url = new URL(String.format("http://%s:%s/pinot-controller/admin", pinotSeverHost, pinotControllerPort));
-      LOGGER.info("checking pinot controller health:{}", url.toString());
-      con = (HttpURLConnection) url.openConnection();
-      con.setRequestMethod("GET");
-      con.setConnectTimeout(timeout * 1000);
-      con.setReadTimeout(timeout * 1000);
-      con.connect();
-      int status = con.getResponseCode();
-
-      return status >= 200 && status <= 206;
-    } finally {
-      if (con != null) {
-        con.disconnect();
-      }
-    }
-  }
-
-  private boolean executePinotBrokerHealthCheck() throws Exception {
-    HttpURLConnection con = null;
-    try {
-      URL url = new URL(String.format("http://%s:%s/health", pinotSeverHost, DEFAULT_PINOT_BROKER_PORT));
-      LOGGER.info("checking pinot broker health:{}", url.toString());
       con = (HttpURLConnection) url.openConnection();
       con.setRequestMethod("GET");
       con.setConnectTimeout(timeout * 1000);
@@ -221,8 +185,8 @@ public class HypertraceUIServerTimerTask extends TimerTask {
   private boolean executeBrokerRegistrationCheck() throws Exception {
     HttpURLConnection con = null;
     try {
-      URL url = new URL(String.format("http://%s:%s/brokers/tenants", pinotSeverHost, pinotControllerPort));
-      LOGGER.info("checking pinot broker registration health:{}", url.toString());
+      URL url = new URL(
+          String.format("http://%s:%s/brokers/tenants", pinotSeverHost, pinotControllerPort));
       con = (HttpURLConnection) url.openConnection();
       con.setRequestMethod("GET");
       con.setConnectTimeout(timeout * 1000);
@@ -232,7 +196,7 @@ public class HypertraceUIServerTimerTask extends TimerTask {
 
       if (status >= 200 && status <= 206) {
         BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
+            new InputStreamReader(con.getInputStream()));
         String inputLine;
         StringBuffer content = new StringBuffer();
         while ((inputLine = in.readLine()) != null) {
