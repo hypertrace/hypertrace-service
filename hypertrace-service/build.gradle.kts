f84aab87aadf52a6f3237b29398a6f8f7ee1e807
Ronak <ronak@traceable.ai>
2020-10-22 17:59:30 +0530

    fixed the no class found error due grpc incompatibile versions

--- hypertrace-service/build.gradle.kts
+++ hypertrace-service/build.gradle.kts
@@ -10,9 +10,9 @@ plugins {
 var hypertraceUiVersion = "latest"
 
 configurations {
-  "runtimeClasspath" {
+  "implementation" {
     resolutionStrategy {
-      force("io.grpc:grpc-netty:1.30.2")
+      force("io.grpc:grpc-netty:1.33.0")
     }
   }
 }
@@ -44,7 +44,7 @@ dependencies {
   runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.13.3")
 
   // GRPC
-  runtimeOnly("io.grpc:grpc-netty:1.30.2")
+  runtimeOnly("io.grpc:grpc-netty:1.33.0")
 
   // Config
   implementation("com.typesafe:config:1.4.0")
