1d48949c2e56222a39581c6145b122e0d2777465
Ravi Singal <ravi.singal@traceable.ai>
2020-10-29 22:10:43 +0530

    revert snyk changes

--- hypertrace-service/build.gradle.kts
+++ hypertrace-service/build.gradle.kts
@@ -48,10 +48,6 @@ dependencies {
 
   // Config
   implementation("com.typesafe:config:1.4.0")
-
-  runtimeOnly("com.google.guava:guava:30.0-android") {
-    because("https://snyk.io/vuln/SNYK-JAVA-COMGOOGLEGUAVA-1015415")
-  }
 }
 
 application {
