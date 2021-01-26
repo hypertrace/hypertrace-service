f81e661dc3ce58d86c9d1ee3ff92718aa3aa64e2
Rishabh Singh <rishabh@traceable.ai>
2021-01-26 22:54:42 +0530

    Update attribute-service build

--- attribute-service/build.gradle.kts
+++ attribute-service/build.gradle.kts
@@ -1,19 +1,8 @@
-import org.hypertrace.gradle.publishing.HypertracePublishExtension
-import org.hypertrace.gradle.publishing.License
-
-plugins {
-  id("org.hypertrace.repository-plugin") version "0.1.2"
-  id("org.hypertrace.ci-utils-plugin") version "0.1.1"
-  id("org.hypertrace.publish-plugin") version "0.3.0" apply false
-  id("org.hypertrace.jacoco-report-plugin") version "0.1.0" apply false
-  id("org.hypertrace.integration-test-plugin") version "0.1.0" apply false
-}
-
 subprojects {
   group = "org.hypertrace.core.attribute.service"
   pluginManager.withPlugin("org.hypertrace.publish-plugin") {
-    configure<HypertracePublishExtension> {
-      license.set(License.APACHE_2_0)
+    configure<org.hypertrace.gradle.publishing.HypertracePublishExtension> {
+      license.set(org.hypertrace.gradle.publishing.License.APACHE_2_0)
     }
   }
 
