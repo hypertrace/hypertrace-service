be36583f34b59d656b0573385bb08ab59eb62110
Rishabh <rishabh@traceable.ai>
2021-02-04 20:25:05 +0530

    feat: Hypertrace data config service (#66)
    Continuation of #63
    
    Add a service which combines entity, attribute and config service
    
    Tested the ht setup locally with hotrod app (using https://github.com/hypertrace/hypertrace/compare/hypertrace-data-config-service)
    
    attribute-entity-config-bootstrapper-fnwzt        0/1     Completed   0          77s
    hypertrace-data-config-service-6cdf849c8b-b9trl   1/1     Running     0          78s
    hypertrace-data-query-service-78b78f7775-x5ls8    1/1     Running     0          77s
    all-views-creation-job-8mvbl                      0/1     Completed   0          94s
    all-views-generator-f6c98ddfc-c25zt               1/1     Running     0          78s
    hypertrace-graphql-service-8cfbdf85-7wdc9         1/1     Running     0          78s
    hypertrace-kafka-topics-creator-7dh5d             0/1     Completed   0          2m41s
    hypertrace-oc-collector-685c846b99-7hnfn          1/1     Running     0          77s
    hypertrace-trace-enricher-7bd7786cf-xxhdl         1/1     Running     0          78s
    hypertrace-ui-cdbc96b65-mn76v                     1/1     Running     0          78s
    kafka-0                                           1/1     Running     0          34m
    mongo-0                                           1/1     Running     0          34m
    pinot-broker-0                                    1/1     Running     2          34m
    pinot-controller-0                                1/1     Running     0          34m
    pinot-minion-0                                    1/1     Running     2          34m
    pinot-server-0                                    1/1     Running     2          34m
    raw-spans-grouper-0                               1/1     Running     0          77s
    schema-registry-567c8467fb-gxpl9                  1/1     Running     0          34m
    span-normalizer-58c867d84c-pf5wt                  1/1     Running     0          77s
    view-generation-kafka-topics-creator-ck9hv        0/5     Completed   0          105s
    zookeeper-0                                       1/1     Running     0          34m

---
+++ hypertrace-data-config-service/src/main/java/org/hypertrace/service/HypertraceDataConfigService.java
@@ -0,0 +1,123 @@
+package org.hypertrace.service;
+
+import com.typesafe.config.Config;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import java.io.IOException;
+import org.hypertrace.config.service.ConfigServicesFactory;
+import org.hypertrace.core.attribute.service.AttributeServiceImpl;
+import org.hypertrace.core.documentstore.Datastore;
+import org.hypertrace.core.documentstore.DatastoreProvider;
+import org.hypertrace.core.grpcutils.server.InterceptorUtil;
+import org.hypertrace.core.serviceframework.PlatformService;
+import org.hypertrace.core.serviceframework.config.ConfigClient;
+import org.hypertrace.core.serviceframework.config.ConfigUtils;
+import org.hypertrace.entity.data.service.EntityDataServiceImpl;
+import org.hypertrace.entity.query.service.EntityQueryServiceImpl;
+import org.hypertrace.entity.service.EntityServiceConfig;
+import org.hypertrace.entity.type.service.v2.EntityTypeServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HypertraceDataConfigService extends PlatformService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(HypertraceDataConfigService.class);
+
+  private static final String PORT_PATH = "service.port";
+
+  private static final String CLUSTER_NAME = "cluster.name";
+  private static final String POD_NAME = "pod.name";
+  private static final String CONTAINER_NAME = "container.name";
+
+  private static final String ENTITY_SERVICE_NAME = "entity-service";
+  private static final String ATTRIBUTE_SERVICE_NAME = "attribute-service";
+  private static final String CONFIG_SERVICE_NAME = "config-service";
+
+  private static final String ENTITY_SERVICE_ENTITY_SERVICE_CONFIG = "entity.service.config";
+  private Server server;
+
+  public HypertraceDataConfigService(ConfigClient configClient) {
+    super(configClient);
+  }
+
+  @Override
+  protected void doInit() {
+    int port = getAppConfig().getInt(PORT_PATH);
+
+    final ServerBuilder<?> serverBuilder = ServerBuilder.forPort(port);
+
+    // Attribute service
+    final Config attributeServiceAppConfig = getServiceConfig(ATTRIBUTE_SERVICE_NAME);
+    serverBuilder.addService(
+        InterceptorUtil.wrapInterceptors(new AttributeServiceImpl(attributeServiceAppConfig)));
+
+    // Entity service
+    final Config entityServiceAppConfig = getServiceConfig(ENTITY_SERVICE_NAME);
+    final EntityServiceConfig entityServiceConfig = new EntityServiceConfig(
+        entityServiceAppConfig.getConfig(ENTITY_SERVICE_ENTITY_SERVICE_CONFIG));
+    final Config dataStoreConfig =
+        entityServiceConfig.getDataStoreConfig(entityServiceConfig.getDataStoreType());
+    final Datastore datastore =
+        DatastoreProvider.getDatastore(entityServiceConfig.getDataStoreType(), dataStoreConfig);
+
+    ManagedChannel localChannel =
+        ManagedChannelBuilder.forAddress("localhost", port).usePlaintext().build();
+
+    serverBuilder.addService(InterceptorUtil.wrapInterceptors(new org.hypertrace.entity.type.service.EntityTypeServiceImpl(datastore)))
+        .addService(InterceptorUtil.wrapInterceptors(new EntityTypeServiceImpl(datastore)))
+        .addService(InterceptorUtil.wrapInterceptors(new EntityDataServiceImpl(datastore, localChannel)))
+        .addService(InterceptorUtil
+            .wrapInterceptors(new EntityQueryServiceImpl(datastore, entityServiceAppConfig)));
+
+    // Config service
+    ConfigServicesFactory.buildAllConfigServices(
+        getServiceConfig(CONFIG_SERVICE_NAME), port, getLifecycle())
+        .stream()
+        .map(InterceptorUtil::wrapInterceptors)
+        .forEach(serverBuilder::addService);
+
+    this.server = serverBuilder.build();
+  }
+
+  private Config getServiceConfig(String serviceName) {
+    return configClient.getConfig(
+        serviceName,
+        ConfigUtils.getEnvironmentProperty(CLUSTER_NAME),
+        ConfigUtils.getEnvironmentProperty(POD_NAME),
+        ConfigUtils.getEnvironmentProperty(CONTAINER_NAME));
+  }
+
+  @Override
+  protected void doStart() {
+    LOGGER.info("Starting Hypertrace Data Config Service");
+    try {
+      server.start();
+      server.awaitTermination();
+    } catch (IOException e) {
+      LOGGER.error("Fail to start the server.");
+      throw new RuntimeException(e);
+    } catch (InterruptedException ie) {
+      Thread.currentThread().interrupt();
+      throw new RuntimeException(ie);
+    }
+  }
+
+  @Override
+  protected void doStop() {
+    LOGGER.info("Shutting down Hypertrace Data Config Service: {}", getServiceName());
+    while (!server.isShutdown()) {
+      server.shutdown();
+      try {
+        Thread.sleep(100);
+      } catch (InterruptedException ignore) {
+      }
+    }
+  }
+
+  @Override
+  public boolean healthCheck() {
+    return true;
+  }
+}
