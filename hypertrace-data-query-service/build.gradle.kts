394d7633f3dfbce28da3d10b9e10d6d1dafeda42
Ronak <ronak@traceable.ai>
2021-02-16 12:36:58 +0530

    chore: bump doc-store version to include postgres impl of exits, not-exsits and neq

--- hypertrace-data-query-service/build.gradle.kts
+++ hypertrace-data-query-service/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
   implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.16")
   implementation("org.hypertrace.core.grpcutils:grpc-server-utils:0.3.0")
   implementation("org.hypertrace.core.grpcutils:grpc-client-utils:0.3.0")
-  implementation("org.hypertrace.core.documentstore:document-store:0.4.4")
+  implementation("org.hypertrace.core.documentstore:document-store:0.5.3")
 
   // Logging
   implementation("org.slf4j:slf4j-api:1.7.30")
