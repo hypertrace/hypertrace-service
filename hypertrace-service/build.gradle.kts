1af85c8b2a0a09113fe94033c532b0b2ba6b276a
Ronak <ronak@traceable.ai>
2022-03-17 12:33:31 +0530

    upgarde document-store lib version of local projects

--- hypertrace-service/build.gradle.kts
+++ hypertrace-service/build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
   implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.29")
   implementation("org.hypertrace.core.grpcutils:grpc-server-utils:0.6.1")
   implementation("org.hypertrace.core.grpcutils:grpc-client-utils:0.6.1")
-  implementation("org.hypertrace.core.documentstore:document-store:0.6.4")
+  implementation("org.hypertrace.core.documentstore:document-store:0.6.14")
   implementation("com.typesafe:config:1.4.1")
   implementation("org.slf4j:slf4j-api:1.7.32")
 
